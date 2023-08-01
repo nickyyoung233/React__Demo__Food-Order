e149463f526b1d54e6933e47c5a697d16088bc39
16684814105 <402150786@qq.com>
2023-08-01 10:21:20 +0800

    build structure

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# React__Demo__Food-Order
- demo for using react
+# React**Demo**Food-Order
+
+demo for using react
+
+## set structure for project
+
+## Q&A
+
+### diff with space-around & space-between
+
+### input why aligh with text & overflow the line
--- src/App.js
+++ src/App.js
@@ -1,7 +1,54 @@
 function App() {
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <header className="flex header">
+        <h1>订餐Demo</h1>
+        <div className="flex cart-button">
+          <i>ICON</i>
+          <span>购物车</span>
+          <div>NUMBERS</div>
+        </div>
+      </header>
+
+      <div className="flex background">
+        <div className="flex intro">
+          <h2>餐厅简介</h2>
+          <p>餐厅简介01</p>
+          <p>餐厅简介02</p>
+        </div>
+        <ul className="flex order-list">
+          <li className="flex order-item">
+            <div className="order-info">
+              <h3>Sushi</h3>
+              <p>Sushi introduction</p>
+              <p>￥100</p>
+            </div>
+            <div className="flex order-num">
+              <div className="order-count">
+                <label htmlFor="food1">Amount</label>
+                <input type="number" min="0" id="food1" />
+              </div>
+              <button className="button" type="button">
+                + Add
+              </button>
+            </div>
+          </li>
+          <li className="flex order-item">
+            <div className="order-info">
+              <h3>Sushi</h3>
+              <p>Sushi introduction</p>
+              <p>￥100</p>
+            </div>
+            <div className="order-num">
+              <label htmlFor="">Amount</label>
+              <input type="number" min="0" />
+              <button type="button">+ Add</button>
+            </div>
+          </li>
+        </ul>
+      </div>
+
+      <div className="cargo"></div>
     </div>
   );
 }
